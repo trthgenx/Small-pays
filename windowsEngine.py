cce3cd54a995bdaa8d088721455941ae678d676a
AntibioticsABX <62002585+AntibioticsABX@users.noreply.github.com>
2020-10-25 22:11:01 +0630

    window payload added2

--- windowsEngine.py
+++ windowsEngine.py
@@ -1,13 +1,13 @@
-import os
-import socket
-import getpass
-
-
-
-class WindowsPayloadEngine():
-
-    def big_Deleter(self):
-
-        cwd = os.getcwd()
-
-print(getpass.getuser())
+# import os
+# import socket
+# import getpass
+#
+#
+#
+# class WindowsPayloadEngine():
+#
+#     def big_Deleter(self):
+#
+#         cwd = os.getcwd()
+#
+# print(getpass.getuser())
